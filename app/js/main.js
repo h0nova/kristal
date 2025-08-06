ea53e4cb8652db14f49ec45b453cca7da2f9e69f
h0nova <timalsyenko502@gmail.com>
2025-08-06 22:41:40 +0300

    simled catalog-slider

--- app/js/main.js
+++ app/js/main.js
@@ -15,35 +15,33 @@ $(".catalog__btn").on("click", function () {
   $(".catalog__btn").toggleClass("catalog__btn--active");
 });
 
+$(".catalog-slider__list").slick({
+  slidesToShow: 1,
+  arrows: true,
+  dots: true,
+});
+
 $(".catalog__item, .catalog-slider__btn").on("click", function () {
-  if (!openCatalogSlider) {
-    var imagesCount = $(this).data("count");
-    var itemId = $(this).attr("id");
-    itemId = itemId.split("-")[1];
+  // if (!openCatalogSlider) {
+  //   var imagesCount = $(this).data("count");
+  //   var itemId = $(this).attr("id");
+  //   itemId = itemId.split("-")[1];
 
-    console.log(itemId);
+  // for (var i = 1; i < imagesCount + 1; i++) {
+  //   $(".catalog-slider__list").append(
+  //     `<li class="catalog-slider__item">
+  //    <img class="catalog-slider__img" src="images/catalog/item_${itemId}/image_${i}.png" alt="">
+  //  </li>`
+  //   );
+  // }
 
-    for (var i = 1; i < imagesCount + 1; i++) {
-      $(".catalog-slider__list").append(
-        `<li class="catalog-slider__item">
-       <img class="catalog-slider__img" src="images/catalog/item_${itemId}/image_${i}.png" alt="">
-     </li>`
-      );
-    }
+  //   openCatalogSlider = true;
+  // } else {
+  //   $(".catalog-slider__list").empty();
+  //   $(".catalog-slider__list").removeClass("slick-initialized slick-slider");
 
-    $(".catalog-slider__list").slick({
-      slidesToShow: 1,
-      arrows: true,
-      dots: true,
-    });
-
-    openCatalogSlider = true;
-  } else {
-    $(".catalog-slider__list").empty();
-    $(".catalog-slider__list").removeClass("slick-initialized slick-slider");
-
-    openCatalogSlider = false;
-  }
+  //   openCatalogSlider = false;
+  // }
 
   $(".catalog-slider").toggleClass("catalog-slider--active");
 });
